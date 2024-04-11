54dc40766b493ced3631da3b6c4e8f7e1d4fa357
Dmitrii <dimon-filatov@yandex.ru>
2024-04-11 21:25:04 +0300

    Lesson 17 completed

--- project_tracker/quality_control/views.py
+++ project_tracker/quality_control/views.py
@@ -1,3 +1,26 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.urls import reverse
 
-# Create your views here.
+
+def index(request):
+    bugs_page_url = reverse('quality_сontrol:bugs')
+    features_page_url = reverse('quality_сontrol:features')
+    html = f"<h1>Система контроля качества</h1><a href='{bugs_page_url}'>Список всех багов</a><br><a href='{features_page_url}'>Запросы на улучшение</a>"
+    return HttpResponse(html)
+
+
+def bug_list(request):
+    return HttpResponse("<h1>Cписок отчетов об ошибках</h1>")
+
+
+def bug_detail(request, bug_id):
+    return HttpResponse(f"Детали бага {bug_id}")
+
+
+def feature_list(request):
+    return HttpResponse("<h1>Список запросов на улучшение</h1>")
+
+
+def feature_id_detail(request, feature_id):
+    return HttpResponse(f"Детали улучшения {feature_id}")
--- project_tracker/tasks/views.py
+++ project_tracker/tasks/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.urls import reverse
 
-# Create your views here.
+
+def index(request):
+    another_page_url = reverse('tasks:another_page')
+    quality_control_page_url = reverse('quality_сontrol:index')
+    html = f"<h1>Страница приложения Tasks</h1><a href='{another_page_url}'>Перейти на другую страницу</a><br><a href='{quality_control_page_url}'>Перейти на страницу багов и предложений</a>"
+    return HttpResponse(html)
+
+
+def another_page(request):
+    return HttpResponse("Это другая страница приложения tasks.")
